f6ee8c357c0e5143c19416420e00602a82f4defd
yoni <yoni@yonis-MacBook-Air.local>
2023-01-21 16:17:14 +0200

    add colors

--- gpt.sh
+++ gpt.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+readonly RED="$(tput setaf 1)" 
+readonly GREEN="$(tput setaf 2)" 
+readonly BLUE="$(tput setaf 4)" 
+readonly BOLD="$(tput bold)" 
+readonly BLINK="$(tput bold)" 
+readonly RESET="$(tput sgr0)"
+
+
 while true; do
     read -p "You: " input
     if [ "$input" == "exit" ]; then
         break
     fi
+
 		echo -e "\n"		
-		python query.py  "$input"
-	
-		echo -e "\n-----------------------"
+		response=$(python query.py "$input")
+		echo "${GREEN}$response${RESET}"
+		echo -e "\n"
 done
